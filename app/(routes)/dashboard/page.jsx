1be86ec6fd01a16234fe6e67e6a831593da3a62b
Manas Bhole <62958063+ManasBhole@users.noreply.github.com>
2024-09-25 01:23:16 -0500

    Updates in page

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { getAuth } from "@clerk/nextjs/server";
+"use client";
+import React, { useEffect, useState } from "react";
+import { UserButton, useUser } from "@clerk/nextjs";
 import CardInfo from "./_components/CardInfo";
 import { db } from "@/utils/dbConfig";
 import { desc, eq, getTableColumns, sql } from "drizzle-orm";
@@ -7,68 +8,80 @@ import { Budgets, Expenses, Incomes } from "@/utils/schema";
 import BarChartDashboard from "./_components/BarChartDashboard";
 import BudgetItem from "./budgets/_components/BudgetItem";
 import ExpenseListTable from "./expenses/_components/ExpenseListTable";
+function Dashboard() {
+  const { user } = useUser();
 
-export async function getServerSideProps(context) {
-  // Fetch the authenticated user's session
-  const { userId } = getAuth(context.req);
+  const [budgetList, setBudgetList] = useState([]);
+  const [incomeList, setIncomeList] = useState([]);
+  const [expensesList, setExpensesList] = useState([]);
+  useEffect(() => {
+    user && getBudgetList();
+  }, [user]);
+  /**
+   * used to get budget List
+   */
+  const getBudgetList = async () => {
+    const result = await db
+      .select({
+        ...getTableColumns(Budgets),
 
-  if (!userId) {
-    return {
-      redirect: {
-        destination: "/login", // Redirect to login if not authenticated
-        permanent: false,
-      },
-    };
-  }
-
-  // Fetch budgets, incomes, and expenses data based on the user
-  const budgetList = await db
-    .select({
-      ...getTableColumns(Budgets),
-      totalSpend: sql`sum(${Expenses.amount})`.mapWith(Number),
-      totalItem: sql`count(${Expenses.id})`.mapWith(Number),
-    })
-    .from(Budgets)
-    .leftJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
-    .where(eq(Budgets.createdBy, userId))
-    .groupBy(Budgets.id)
-    .orderBy(desc(Budgets.id));
-
-  const incomeList = await db
-    .select({
-      ...getTableColumns(Incomes),
-      totalAmount: sql`SUM(CAST(${Incomes.amount} AS NUMERIC))`.mapWith(Number),
-    })
-    .from(Incomes)
-    .groupBy(Incomes.id);
-
-  const expensesList = await db
-    .select({
-      id: Expenses.id,
-      name: Expenses.name,
-      amount: Expenses.amount,
-      createdAt: Expenses.createdAt,
-    })
-    .from(Budgets)
-    .rightJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
-    .where(eq(Budgets.createdBy, userId))
-    .orderBy(desc(Expenses.id));
-
-  return {
-    props: {
-      budgetList,
-      incomeList,
-      expensesList,
-    },
+        totalSpend: sql`sum(${Expenses.amount})`.mapWith(Number),
+        totalItem: sql`count(${Expenses.id})`.mapWith(Number),
+      })
+      .from(Budgets)
+      .leftJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress))
+      .groupBy(Budgets.id)
+      .orderBy(desc(Budgets.id));
+    setBudgetList(result);
+    getAllExpenses();
+    getIncomeList();
   };
-}
 
-function Dashboard({ budgetList, incomeList, expensesList }) {
+  /**
+   * Get Income stream list
+   */
+  const getIncomeList = async () => {
+    try {
+      const result = await db
+        .select({
+          ...getTableColumns(Incomes),
+          totalAmount: sql`SUM(CAST(${Incomes.amount} AS NUMERIC))`.mapWith(
+            Number
+          ),
+        })
+        .from(Incomes)
+        .groupBy(Incomes.id); // Assuming you want to group by ID or any other relevant column
+
+      setIncomeList(result);
+    } catch (error) {
+      console.error("Error fetching income list:", error);
+    }
+  };
+
+  /**
+   * Used to get All expenses belong to users
+   */
+  const getAllExpenses = async () => {
+    const result = await db
+      .select({
+        id: Expenses.id,
+        name: Expenses.name,
+        amount: Expenses.amount,
+        createdAt: Expenses.createdAt,
+      })
+      .from(Budgets)
+      .rightJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress.emailAddress))
+      .orderBy(desc(Expenses.id));
+    setExpensesList(result);
+  };
+
   return (
     <div className="p-8 bg-">
-      <h2 className="font-bold text-4xl">Hi, 👋</h2>
+      <h2 className="font-bold text-4xl">Hi, {user?.fullName} 👋</h2>
       <p className="text-gray-500">
-        Here's what's happening with your money, Let's manage your expenses
+        Here's what happenning with your money, Lets Manage your expense
       </p>
 
       <CardInfo budgetList={budgetList} incomeList={incomeList} />
@@ -78,7 +91,7 @@ function Dashboard({ budgetList, incomeList, expensesList }) {
 
           <ExpenseListTable
             expensesList={expensesList}
-            refreshData={() => {} /* handle refreshing if needed */}
+            refreshData={() => getBudgetList()}
           />
         </div>
         <div className="grid gap-5">
@@ -89,8 +102,7 @@ function Dashboard({ budgetList, incomeList, expensesList }) {
               ))
             : [1, 2, 3, 4].map((item, index) => (
                 <div
-                  key={index}
-                  className="h-[180px] w-full
+                  className="h-[180xp] w-full
                  bg-slate-200 rounded-lg animate-pulse"
                 ></div>
               ))}
